e4b063eea48df8b1d8c22dfa7a337c2ca101a58e
Francisc Munteanu <fmuntean@redhat.com>
2022-10-20 10:28:01 +0200

    retrieve workspace path from apiexport in main function

--- main.go
+++ main.go
@@ -42,7 +42,6 @@ import (
 
 	apisv1alpha1 "github.com/kcp-dev/kcp/pkg/apis/apis/v1alpha1"
 	appstudiov1alpha1 "github.com/redhat-appstudio/api-manager/api/v1alpha1"
-	"github.com/redhat-appstudio/api-manager/controllers"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -121,13 +120,14 @@ func main() {
 		// LeaderElectionReleaseOnCancel: true,
 	}
 
+	apiExportWorkspace := ""
 	if kcpAPIsGroupPresent(restConfig) {
 		setupLog.Info("Looking up virtual workspace URL")
-		cfg, err := restConfigForAPIExport(ctx, restConfig, apiExportName)
+		var cfg *rest.Config
+		cfg, apiExportWorkspace, err = restConfigForAPIExport(ctx, restConfig, apiExportName)
 		if err != nil {
 			setupLog.Error(err, "error looking up virtual workspace URL")
 		}
-
 		setupLog.Info("Using virtual workspace URL", "url", cfg.Host)
 
 		options.LeaderElectionConfig = restConfig
@@ -145,15 +145,26 @@ func main() {
 		}
 	}
 
-	if err = (&controllers.APIManagerReconciler{
-		Client:          mgr.GetClient(),
-		SPWorkspacePath: spWorkspacePath,
-		APIExportName:   apiExportName,
-		ChartPath:       chartPath,
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "APIManager")
-		os.Exit(1)
-	}
+	setupLog.Info("workspace path: " + apiExportWorkspace)
+	setupLog.Info("chart path: " + chartPath)
+	// TODO add WRC integration
+	//if err = (&controllers.ResourceReconciler{
+	//	Client:        mgr.GetClient(),
+	//	Config:        mgr.GetConfig(),
+	//	ChartLocation: chartPath,
+	//  ChartValues: {""}
+	//})
+
+	// TODO enable in order to accept all permision claims for all apibidings
+	//if err = (&controllers.APIManagerReconciler{
+	//	Client:          mgr.GetClient(),
+	//	SPWorkspacePath: spWorkspacePath,
+	//	APIExportName:   apiExportName,
+	//	ChartPath:       chartPath,
+	//}).SetupWithManager(mgr); err != nil {
+	//	setupLog.Error(err, "unable to create controller", "controller", "APIManager")
+	//	os.Exit(1)
+	//}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
@@ -177,47 +188,48 @@ func main() {
 
 // restConfigForAPIExport returns a *rest.Config properly configured to communicate with the endpoint for the
 // APIExport's virtual workspace.
-func restConfigForAPIExport(ctx context.Context, cfg *rest.Config, apiExportName string) (*rest.Config, error) {
+func restConfigForAPIExport(ctx context.Context, cfg *rest.Config, apiExportName string) (*rest.Config, string, error) {
 	scheme := runtime.NewScheme()
 	if err := apisv1alpha1.AddToScheme(scheme); err != nil {
-		return nil, fmt.Errorf("error adding apis.kcp.dev/v1alpha1 to scheme: %w", err)
+		return nil, "", fmt.Errorf("error adding apis.kcp.dev/v1alpha1 to scheme: %w", err)
 	}
 
 	apiExportClient, err := client.New(cfg, client.Options{Scheme: scheme})
 	if err != nil {
-		return nil, fmt.Errorf("error creating APIExport client: %w", err)
+		return nil, "", fmt.Errorf("error creating APIExport client: %w", err)
 	}
 
 	var apiExport apisv1alpha1.APIExport
 
 	if apiExportName != "" {
 		if err := apiExportClient.Get(ctx, types.NamespacedName{Name: apiExportName}, &apiExport); err != nil {
-			return nil, fmt.Errorf("error getting APIExport %q: %w", apiExportName, err)
+			return nil, "", fmt.Errorf("error getting APIExport %q: %w", apiExportName, err)
 		}
 	} else {
 		setupLog.Info("api-export-name is empty - listing")
 		exports := &apisv1alpha1.APIExportList{}
 		if err := apiExportClient.List(ctx, exports); err != nil {
-			return nil, fmt.Errorf("error listing APIExports: %w", err)
+			return nil, "", fmt.Errorf("error listing APIExports: %w", err)
 		}
 		if len(exports.Items) == 0 {
-			return nil, fmt.Errorf("no APIExport found")
+			return nil, "", fmt.Errorf("no APIExport found")
 		}
 		if len(exports.Items) > 1 {
-			return nil, fmt.Errorf("more than one APIExport found")
+			return nil, "", fmt.Errorf("more than one APIExport found")
 		}
 		apiExport = exports.Items[0]
 	}
 
 	if len(apiExport.Status.VirtualWorkspaces) < 1 {
-		return nil, fmt.Errorf("APIExport %q status.virtualWorkspaces is empty", apiExportName)
+		return nil, "", fmt.Errorf("APIExport %q status.virtualWorkspaces is empty", apiExportName)
 	}
 
 	cfg = rest.CopyConfig(cfg)
 	// TODO(ncdc): sharding support
 	cfg.Host = apiExport.Status.VirtualWorkspaces[0].URL
+	apiExportWorkspace := apiExport.Annotations["kcp.dev/cluster"]
 
-	return cfg, nil
+	return cfg, apiExportWorkspace, nil
 }
 
 func kcpAPIsGroupPresent(restConfig *rest.Config) bool {
